ebb84d0025a418c92cdce277c8bf45d0e334bf29
Mike <misha@malyarov.com>
2017-03-16 15:31:32 +0300

    Add test DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToodooApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.export = {
 	mongoose: mongoose
